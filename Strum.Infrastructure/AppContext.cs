7c3e85395400090718bd92c263aeccbe3ce69853
vayuprog <vaniayushchuk.15@gmail.com>
2023-11-11 19:57:20 +0200

    first commit

---
+++ Strum.Infrastructure/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Strum.Core.Entities;
+
+namespace Strum.Infrastructure;
+
+public class AppContext:DbContext
+{
+	public AppContext()
+	{
+	}
+	public DbSet<User> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+		optionsBuilder.UseNpgsql("Host = localhost; Database = Strum; Username = postgres; Password = admin228");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
